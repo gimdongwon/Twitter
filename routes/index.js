0d139deb59ebbc260446784538df9008ee3fe56d
gimdongwon <ehddnjs8989@gmail.com>
2021-10-27 13:24:13 +0900

    hash tag 기능추가

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Post, User } = require('../models');
+const { Post, User, Hashtag } = require('../models');
 const { isNotLoggedIn, isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -40,6 +40,27 @@ router.get('/', async (req, res, next) => {
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+router.get('/hashtag', async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+    return res.render('main', {
+      title: `${query} | Twitter`,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 
