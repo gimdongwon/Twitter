307158a6c721f5e1f2a9ed9c3300996407952d1e
gimdongwon <ehddnjs8989@gmail.com>
2021-10-27 15:34:17 +0900

    img uploads 기능 추가

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ sequelize
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
